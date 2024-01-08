f4ac8abb81a2d9add638582bd8d98f0d7cb7d75e
Joshua Hiles <joshhiles1@live.co.uk>
2024-01-08 18:42:34 +0000

    test(benchmarking): Add benchmarking project

---
+++ Scientist.Benchmark/ExperimentBenchmarks.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Scientist.Benchmark
+{
+    [MemoryDiagnoser]
+    public class ExperimentBenchmarks
+    {
+        readonly Experiment<int> experiment;
+
+        public ExperimentBenchmarks()
+        {
+            experiment = new Experiment<int>(name: "Instance", control: FortyTwo);
+        }
+
+        [Benchmark]
+        public int Static()
+        {
+            return new Experiment<int>(name: "Static", control: FortyTwo).Run();
+        }
+
+        [Benchmark(Baseline = false)]
+        public int Instance()
+        {
+            return experiment.Run();
+        }
+
+        private static int FortyTwo() => 42;
+    }
+}
